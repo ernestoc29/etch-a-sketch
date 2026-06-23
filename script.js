2c5e00b7f7a2fe1e542c6192c530487781376a8e
ernesto <274116253+ernestoc29@users.noreply.github.com>
2026-06-23 13:53:06 -0400

    add customize grid button functionality

--- script.js
+++ script.js
@@ -7,15 +7,11 @@ for (let i = 0; i < 256; i++) {
     divContainer.appendChild(div);
 };
 
-const squares = document.querySelectorAll(".div");
-
 function changeBackground(event) {
-    event.target.classList.add("hovered");
+    if (event.target.classList.contains("div")) {
+        event.target.classList.add("hovered");
+    };
 };
-
-squares.forEach(square => {
-    square.addEventListener("mouseover", (changeBackground));
-});
 
 function customize() {    
     let squaresPerSide;
@@ -24,8 +20,21 @@ function customize() {
         squaresPerSide = parseInt(prompt("How many squares per side? (Max: 100)"));
     } while (isNaN(squaresPerSide) || squaresPerSide < 1 || squaresPerSide > 100);
 
-    console.log(squaresPerSide)
+    divContainer.replaceChildren();
+
+    for (let i = 0; i < squaresPerSide * squaresPerSide; i++) {
+       const div = document.createElement("div"); 
+       div.classList.add("div");
+
+       div.style.width = (100 / squaresPerSide) + "%";
+       div.style.height = (100 / squaresPerSide) + "%";
+
+       divContainer.appendChild(div);
+    };
+    
     return squaresPerSide;
 };
 
 customizeGrid.addEventListener("click", customize);
+
+divContainer.addEventListener("mousemove", changeBackground);
